c48cb6fc4c0806c138a7c931171e53bb645d534a
Chungwei <b01704060@gmail.com>
2018-11-24 19:11:03 +0900

    File Upload

---
+++ c_to_f.py
@@ -0,0 +1,6 @@
+c = input('The Celsius degree now is: ')
+c = float(c)
+f= c * 9 / 5 + 32
+print('Fahrenheit=', f)
+clothe_layer = 11-f/10
+print('Clothe Layer Suggestion=', int(clothe_layer), 'pieces')
